b890f92b3b33056e16081f2d9d7ecbfb76fc881a
Abdulsalam Shahlol <abdulsalamshahlol@gmail.com>
2020-01-03 19:35:24 +0100

    react env build from scratch and working

---
+++ public/dist/main.79980fbc03812784377e.hot-update.js
@@ -0,0 +1,14 @@
+webpackHotUpdate("main",{
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("throw new Error(\"Module build failed (from ./node_modules/babel-loader/lib/index.js):\\nError: ENOENT: no such file or directory, open 'E:\\\\Personal\\\\Projects\\\\Node\\\\r-maker\\\\src\\\\index.js'\");//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zcmMvaW5kZXguanMuanMiLCJzb3VyY2VzIjpbXSwibWFwcGluZ3MiOiIiLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///./src/index.js\n");
+
+/***/ })
+
+})
